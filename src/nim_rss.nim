c634b3a17074e8b561216155daa7bfd5e0a4d1d2
theansweris4ty2 <mdjd2001@gmail.com>
2026-04-24 21:18:21 -0700

    Third Commit - minor refactor and styling

--- src/nim_rss.nim
+++ src/nim_rss.nim
@@ -1,21 +1,18 @@
 import rss, std/httpclient, std/terminal, std/uri
 proc get_feed () =
   let client = newHttpClient()
-
- 
   for line in lines("feeds.config"): 
     var res = initUri()
     parseUri(line, res)
     var feed = getRSS(line)
-
-    echo "--- Feed Title: ", feed.title, " ---"
-
-    for item in feed.items:
-      let title = item.title
-      stdout.styledWriteline(fgRed, title)
-      echo "\nLink: ", item.link
-      echo "\nDate: ", item.pubDate
-      echo "\nSummary: ", item.description[0..min(100, item.description.len-1)], "..."
+    stdout.styledWriteline( fgBlue, "--- Feed Title: ", feed.title, " --- \n")
+    for  index, item in feed.items:
+      if index < 5:
+        stdout.styledWriteline(fgGreen, item.title )
+        echo "\nDate: ", item.pubDate
+        echo "\nSummary: ", item.description[0..min(100, item.description.len-1)], "... \n"
+        stdout.styledWriteline(fgCyan, item.link)
+        echo "\n"
 
 get_feed()
 
